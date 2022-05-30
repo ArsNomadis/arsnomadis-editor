3f406c485e20e01ca9bc19d95ea209c483ccf92f
Denez Thomas <denezthomas@gmail.com>
2022-05-30 23:16:04 +0200

    Export global styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,11 @@
 <script>
+    import '../global.css'
     import Inspector from '$lib/inspector/Inspector.svelte'
     import Map from '$lib/map/Map.svelte'
-    import ExportButton from '$lib/project/ExportButton.svelte';
+    import ExportButton from '$lib/project/ExportButton.svelte'
 </script>
 
 <Map/>
 <Inspector/>
 <ExportButton />
-
-<style>
-    :global(body) {
-        position: relative;
-        margin: 0%;
-        min-height: 100vh;
-        width: 100%;
-        overflow: hidden;
-    }
-</style>
-
 
