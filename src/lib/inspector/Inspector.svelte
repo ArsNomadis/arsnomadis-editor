7f2a3a520a76b24149c5c8b767cd81fc3abeda83
Denez Thomas <denezthomas@gmail.com>
2022-06-18 02:59:39 +0200

    Add inspector placeholder (about)

--- src/lib/inspector/Inspector.svelte
+++ src/lib/inspector/Inspector.svelte
@@ -1,6 +1,6 @@
 <script>
     import { project } from '../../stores/projectStore.js'
-    import Zone from "./Zone.svelte";
+    import Zone from './Zone.svelte'
 
     let div
 
@@ -9,14 +9,29 @@
     }
 </script>
 
-<div bind:this={div}>
+<div class="container" bind:this={div}>
     {#each $project.zones as zone, i (zone.uuid)}
         <Zone id={i} on:shouldScroll={scrollTo}/>
     {/each}
+
+    {#if $project.zones.length === 0}
+        <div class="about">
+            <h1>Ars Nomadis Editor</h1>
+            <p>
+                📣 Web editor for
+                <a href="https://www.arsnomadis.eu/projets/les-promenades-sonores/" target="_blank">
+                    Ars Nomadis geolocated soundwalks.
+                </a> The editor generates data to be used with Ars Nomadis custom hardware.
+            </p>
+            <p>
+                <i>Click on the map to start creating a soundwalk or use the import button to open an existing project.</i>
+            </p>
+        </div>
+    {/if} 
 </div>
 
 <style>
-    div {
+    .container {
         background-color: white;
         position: fixed;
         top: 0;
@@ -29,5 +44,18 @@
         box-shadow: 0 35px 60px -15px rgba(0, 0, 0, 0.3);
         overflow: hidden;
         overflow-y: scroll;
+    }
+
+    .about {
+        margin: 1.5rem;
+    }
+
+    p {
+        color: grey;
+    }
+
+    a {
+        text-decoration: none;
+        color: black;
     }
 </style>
